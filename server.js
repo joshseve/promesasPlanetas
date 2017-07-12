88688b2c81d9c4e3cea352ccdcab7a713386e203
joshseve <josh.seve@gmail.com>
2017-07-12 16:43:25 -0500

    Primera version del ejercicio de Planetas con PROMESAS

---
+++ server.js
@@ -0,0 +1,10 @@
+var express = require("express");
+var app = express();
+
+app.use("/data", express.static(__dirname + '/data'));
+app.use("/static", express.static(__dirname + '/assets'));
+app.get("/",(req,res)=>{
+  res.sendFile(__dirname+"/index.html")
+})
+
+app.listen(1234);
