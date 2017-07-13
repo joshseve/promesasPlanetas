b0fa3dcacd919ecc5d35dad8f592fc7827be2c8a
joshseve <josh.seve@gmail.com>
2017-07-13 16:49:13 -0500

    Version 2, de Planetas con Promesas

--- assets/js/ejemplo.js
+++ assets/js/ejemplo.js
@@ -14,5 +14,40 @@ function getJSON(url){
 };
 
 getJSON("data/earth-like-results.json")
-.then(function(mensaje){return(getJSON(mensaje.results[0]))})
-.then(function(resultado){console.log(resultado)});
+.then(function(planetas){return(getJSON(planetas.results.forEach(function(planeta){
+  getJSON (planeta).then(function(infoPlanetas){
+    // console.log(infoPlanetas);
+    obtenerDatos(infoPlanetas);
+    console.log(infoPlanetas);
+    })
+  })))
+});
+
+function obtenerDatos(infoPlanetas){
+  var nombre=infoPlanetas.pl_name;
+  var descubrimiento=infoPlanetas.pl_disc;
+  mostrarPlaneta(nombre, descubrimiento);
+};
+
+function mostrarPlaneta(nombre, descubrimiento){
+    var contenedor = document.getElementById("tarjetaPlaneta");
+    var divCard = document.createElement("div");
+      divCard.className="card col s12 m6";
+    var divCardImage = document.createElement("div");
+    var imagen = document.createElement("img");
+    imagen.src= "static/img/3.jpg";
+    var divCardContenido = document.createElement("div");
+    var nombrePlaneta = document.createElement("h2");
+    nombrePlaneta.innerText = nombre;
+    var infoPlaneta = document.createElement("p");
+    infoPlaneta.innerText = descubrimiento;
+
+
+    divCardImage.appendChild(imagen);
+    divCard.appendChild(divCardImage);
+    divCardContenido.appendChild(nombrePlaneta);
+    divCardContenido.appendChild(infoPlaneta);
+    divCard.appendChild(divCardContenido);
+    contenedor.appendChild(divCard);
+
+}
--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 var express = require("express");
+var path = require("path");
 var app = express();
 
-app.use("/data", express.static(__dirname + '/data'));
-app.use("/static", express.static(__dirname + '/assets'));
+app.use('/static', express.static(path.join(__dirname,'node_modules')));
+app.use('/static', express.static(__dirname + '/assets'));
+app.use('/data', express.static(__dirname + '/data'));
+
 app.get("/",(req,res)=>{
   res.sendFile(__dirname+"/index.html")
 })
